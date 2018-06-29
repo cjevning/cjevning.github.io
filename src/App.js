74fd604733f2c6377cf8aceeb184f56d5334c12b
Conner Jevning <cjevning@gmail.com>
2018-06-29 00:27:23 -0700

    it`s something :man_shrugging:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBasketballBall, faStopwatch, faBan } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +9,25 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <FontAwesomeIcon className="App-logo" icon={faBasketballBall} size="4x" color="orange" />
+          <h1 className="App-title">Hello, I'm Conner</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <FontAwesomeIcon icon={faBan} size="2x" color="whitesmoke" />
+          This is the very beginning of my personal site, more is soon to come!
+          <FontAwesomeIcon icon={faStopwatch} size="2x" color="goldenrod" />
         </p>
+        <div className="Social-container">
+          <a href="https://linkedin.com/in/cjevning">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" color="whitesmoke" />
+          </a>
+          <a href="https://twitter.com/cjevning">
+            <FontAwesomeIcon icon={faTwitter} size="2x" color="whitesmoke" />
+          </a>
+          <a href="https://github.com/cjevning">
+            <FontAwesomeIcon icon={faGithub} size="2x" color="whitesmoke" />
+          </a>
+        </div>
       </div>
     );
   }
