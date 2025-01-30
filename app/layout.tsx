eaa0ce71c8d8260c4ac60f1ee51762388bb3e1ae
Conner Jevning <cjevning@gmail.com>
2025-01-30 10:57:22 -0600

    again

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="w-screen h-screen h-dvh">
+    <html lang="en" className="w-screen h-dvh">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
